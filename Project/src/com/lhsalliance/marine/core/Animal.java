643d0ff2fd37728e604f615b06f4553468f2dfbd
Will Stephenson <absorr@live.com>
2014-11-12 20:24:40 -0500

    Created package and a basic class

---
+++ Project/src/com/lhsalliance/marine/core/Animal.java
@@ -0,0 +1,16 @@
+package com.lhsalliance.marine.core;
+
+public class Animal 
+{
+	public static enum Age 
+	{
+		CHILD, TEEN, ADULT
+	}
+	
+	public Age age;
+	
+	public Animal()//TODO: add data required
+	{
+		
+	}
+}
