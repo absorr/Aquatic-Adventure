18c613ae9d7baa5ff9d810297bd5a1018d4a5197
Will Stephenson <absorr@live.com>
2014-11-13 10:20:17 -0500

    Started on basic coding
    - Created Animal class and 2 enums
    - Created model class and loader
    - Started on Biome and HandlerInput

---
+++ Project/src/com/lhsalliance/marine/core/HandlerInput.java
@@ -0,0 +1,28 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.lhsalliance.marine.core;
+
+import com.jme3.input.controls.ActionListener;
+import com.jme3.input.controls.AnalogListener;
+
+/**
+ *
+ * @author Will
+ */
+public class HandlerInput 
+{
+    /**Use ActionListener to respond to pressed/released inputs (key presses, mouse clicks) */ 
+    private ActionListener actionListener = new ActionListener(){
+        public void onAction(String name, boolean pressed, float tpf){
+            System.out.println(name + " = " + pressed);
+        }
+    };
+    /** Use AnalogListener to respond to continuous inputs (key presses, mouse clicks) */
+    private AnalogListener analogListener = new AnalogListener() {
+       public void onAnalog(String name, float value, float tpf) {
+           System.out.println(name + " = " + value);
+       }
+   };
+}
--- Project/src/com/lhsalliance/marine/core/Main.java
+++ Project/src/com/lhsalliance/marine/core/Main.java
@@ -6,7 +6,15 @@ import com.jme3.math.ColorRGBA;
 import com.jme3.math.Vector3f;
 import com.jme3.renderer.RenderManager;
 import com.jme3.scene.Geometry;
+import com.jme3.scene.Spatial;
 import com.jme3.scene.shape.Box;
+import com.jme3.system.AppSettings;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.imageio.ImageIO;
 
 /**
  * test
@@ -14,9 +22,31 @@ import com.jme3.scene.shape.Box;
  */
 public class Main extends SimpleApplication {
 
+    public static Main game;
+    
     public static void main(String[] args) {
         Main app = new Main();
+        AppSettings cfg = new AppSettings(true);
+        cfg.setFrameRate(60); // set to less than or equal screen refresh rate
+        cfg.setVSync(true);   // prevents page tearing
+        cfg.setFrequency(60); // set to screen refresh rate
+        cfg.setResolution(1024, 768);   
+        cfg.setFullscreen(true); 
+        cfg.setSamples(2);    // anti-aliasing
+        cfg.setTitle("Aquatic Adventure"); // branding: window name
+        try {
+          // Branding: window icon
+          cfg.setIcons(new BufferedImage[]{ImageIO.read(new File("assets/Interface/icon.gif"))});
+        } catch (IOException ex) {
+          Logger.getLogger(Main.class.getName()).log(Level.SEVERE, "Icon missing.", ex);
+        }
+        // branding: load splashscreen from assets
+        cfg.setSettingsDialogImage("Interface/MySplashscreen.png"); 
+        //app.setShowSettings(false); // or don't display splashscreen
+        app.setSettings(cfg);
         app.start();
+        
+        game = app;
     }
 
     @Override
@@ -39,5 +69,16 @@ public class Main extends SimpleApplication {
     @Override
     public void simpleRender(RenderManager rm) {
         //TODO: add render code
+    }
+    
+    public Spatial loadModel(Model m)
+    {
+        /** Load a model. Uses model and texture from jme3-test-data library! */ 
+        Spatial teapot = Main.game.assetManager.loadModel(m.pathModel);
+        Material defaultMat = new Material( assetManager, m.pathMaterial);
+        teapot.setMaterial(defaultMat);
+        rootNode.attachChild(teapot);
+        
+        return teapot;
     }
 }
