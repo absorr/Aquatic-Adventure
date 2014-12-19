93c7a87d6c82a24c0b30b05afb7e55f30072945e
Will Stephenson <absorr@live.com>
2014-12-19 16:40:22 -0500

    Started on interaction (I got clicking on stuff working).

---
+++ Squilly/src/com/lhsalliance/aquatic/core/InteractionHandler.java
@@ -0,0 +1,161 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.lhsalliance.aquatic.core;
+
+import com.jme3.app.SimpleApplication;
+import com.jme3.collision.CollisionResult;
+import com.jme3.collision.CollisionResults;
+import com.jme3.font.BitmapText;
+import com.jme3.input.KeyInput;
+import com.jme3.input.MouseInput;
+import com.jme3.input.controls.ActionListener;
+import com.jme3.input.controls.AnalogListener;
+import com.jme3.input.controls.KeyTrigger;
+import com.jme3.input.controls.MouseButtonTrigger;
+import com.jme3.light.DirectionalLight;
+import com.jme3.material.Material;
+import com.jme3.math.ColorRGBA;
+import com.jme3.math.Ray;
+import com.jme3.math.Vector2f;
+import com.jme3.math.Vector3f;
+import com.jme3.scene.Geometry;
+import com.jme3.scene.Node;
+import com.jme3.scene.Spatial;
+import com.jme3.scene.shape.Box;
+import com.jme3.scene.shape.Sphere;
+
+/**
+ *
+ * @author Will
+ */
+public class InteractionHandler extends SimpleApplication
+{
+  public static void main(String[] args) {
+    InteractionHandler app = new InteractionHandler();
+    app.start();
+  }
+  private Node shootables;
+  private Geometry mark;
+ 
+  @Override
+  public void simpleInitApp() {
+    initCrossHairs(); // a "+" in the middle of the screen to help aiming
+    initKeys();       // load custom key mappings
+    initMark();       // a red sphere to mark the hit
+    
+    inputManager.setCursorVisible(true);
+    flyCam.setEnabled(false);
+ 
+    /** create four colored boxes and a floor to shoot at: */
+    shootables = new Node("Shootables");
+    rootNode.attachChild(shootables);
+    shootables.attachChild(makeCube("a Dragon", -2f, 0f, 1f));
+    shootables.attachChild(makeCube("a tin can", 1f, -2f, 0f));
+    shootables.attachChild(makeCube("the Sheriff", 0f, 1f, -2f));
+    shootables.attachChild(makeCube("the Deputy", 1f, 0f, -4f));
+    shootables.attachChild(makeFloor());
+    shootables.attachChild(makeCharacter());
+  }
+ 
+  /** Declaring the "Shoot" action and mapping to its triggers. */
+  private void initKeys() {
+    inputManager.addMapping("Shoot",
+      new KeyTrigger(KeyInput.KEY_SPACE), // trigger 1: spacebar
+      new MouseButtonTrigger(MouseInput.BUTTON_LEFT)); // trigger 2: left-button click
+    inputManager.addListener(analogListener, "Shoot");
+  } /** Map one or more inputs to an action */
+
+ private AnalogListener analogListener = new AnalogListener() {
+    public void onAnalog(String name, float intensity, float tpf) {
+      if (name.equals("Shoot")) {
+        // Reset results list.
+        CollisionResults results = new CollisionResults();
+        // Convert screen click to 3d position
+        Vector2f click2d = inputManager.getCursorPosition();
+        Vector3f click3d = cam.getWorldCoordinates(new Vector2f(click2d.x, click2d.y), 0f).clone();
+        Vector3f dir = cam.getWorldCoordinates(new Vector2f(click2d.x, click2d.y), 1f).subtractLocal(click3d).normalizeLocal();
+        // Aim the ray from the clicked spot forwards.
+        Ray ray = new Ray(click3d, dir);
+        // Collect intersections between ray and all nodes in results list.
+        rootNode.collideWith(ray, results);
+        // (Print the results so we see what is going on:)
+        for (int i = 0; i < results.size(); i++) {
+          // (For each “hit”, we know distance, impact point, geometry.)
+          float dist = results.getCollision(i).getDistance();
+          Vector3f pt = results.getCollision(i).getContactPoint();
+          String target = results.getCollision(i).getGeometry().getName();
+          System.out.println("Selection #" + i + ": " + target + " at " + pt + ", " + dist + " WU away.");
+        }
+        // Use the results -- we rotate the selected geometry.
+        if (results.size() > 0) {
+          // The closest result is the target that the player picked:
+          Geometry target = results.getClosestCollision().getGeometry();
+          // Here comes the action:
+          if (target.getName().equals("Red Box")) {
+            target.rotate(0, -intensity, 0);
+          } else if (target.getName().equals("Blue Box")) {
+            target.rotate(0, intensity, 0);
+          }
+        }
+      } // else if ...
+    }
+  };
+ 
+  /** A cube object for target practice */
+  protected Geometry makeCube(String name, float x, float y, float z) {
+    Box box = new Box(1, 1, 1);
+    Geometry cube = new Geometry(name, box);
+    cube.setLocalTranslation(x, y, z);
+    Material mat1 = new Material(assetManager, "Common/MatDefs/Misc/Unshaded.j3md");
+    mat1.setColor("Color", ColorRGBA.randomColor());
+    cube.setMaterial(mat1);
+    return cube;
+  }
+ 
+  /** A floor to show that the "shot" can go through several objects. */
+  protected Geometry makeFloor() {
+    Box box = new Box(15, .2f, 15);
+    Geometry floor = new Geometry("the Floor", box);
+    floor.setLocalTranslation(0, -4, -5);
+    Material mat1 = new Material(assetManager, "Common/MatDefs/Misc/Unshaded.j3md");
+    mat1.setColor("Color", ColorRGBA.Gray);
+    floor.setMaterial(mat1);
+    return floor;
+  }
+ 
+  /** A red ball that marks the last spot that was "hit" by the "shot". */
+  protected void initMark() {
+    Sphere sphere = new Sphere(30, 30, 0.2f);
+    mark = new Geometry("BOOM!", sphere);
+    Material mark_mat = new Material(assetManager, "Common/MatDefs/Misc/Unshaded.j3md");
+    mark_mat.setColor("Color", ColorRGBA.Red);
+    mark.setMaterial(mark_mat);
+  }
+ 
+  /** A centred plus sign to help the player aim. */
+  protected void initCrossHairs() {
+    setDisplayStatView(false);
+    guiFont = assetManager.loadFont("Interface/Fonts/Default.fnt");
+    BitmapText ch = new BitmapText(guiFont, false);
+    ch.setSize(guiFont.getCharSet().getRenderedSize() * 2);
+    ch.setText("+"); // crosshairs
+    ch.setLocalTranslation( // center
+      settings.getWidth() / 2 - ch.getLineWidth()/2, settings.getHeight() / 2 + ch.getLineHeight()/2, 0);
+    guiNode.attachChild(ch);
+  }
+ 
+  protected Spatial makeCharacter() {
+    // load a character from jme3test-test-data
+    Spatial golem = assetManager.loadModel("Models/Oto/Oto.mesh.xml");
+    golem.scale(0.5f);
+    golem.setLocalTranslation(-1.0f, -1.5f, -0.6f);
+ 
+    // We must add a light to make the model visible
+    DirectionalLight sun = new DirectionalLight();
+    sun.setDirection(new Vector3f(-0.1f, -0.7f, -1.0f));
+    golem.addLight(sun);
+    return golem;
+  }
+}
